6d6033f1fecc256f578c2ac1991511d2e080df69
Vlad Rindevich <rindevich.vs@gmail.com>
2018-01-05 14:28:17 +0300

    test: add basic tests

---
+++ __tests__/helpers.tsx
@@ -0,0 +1,152 @@
+// tslint:disable:ban-types
+
+import {mount, ReactWrapper} from 'enzyme';
+import * as React from 'react';
+import {OverscanIndicesGetter} from 'react-virtualized/dist/es/Grid';
+import {default as Tree, TreeProps} from '../src';
+import {Node, NodeGetter, RowRendererParams} from '../src/types';
+
+export interface SpecNode {
+  children: SpecNode[];
+  id: string;
+  name: string;
+}
+
+export interface NodeGetterOptions {
+  isOpenedByDefault?: boolean;
+  root?: SpecNode;
+}
+
+export interface TreeRendererOptions {
+  nodeGetter: NodeGetter;
+  rowRenderer?: (params: RowRendererParams) => React.ReactElement<any>;
+}
+
+// Override default behavior of overscanning by at least 1 (for accessibility)
+// Because it makes simpler tests below
+export const overscanIndicesGetter: OverscanIndicesGetter = ({startIndex, stopIndex}) => ({
+  overscanStartIndex: startIndex,
+  overscanStopIndex: stopIndex,
+});
+
+export function createNodeCreator(maxDepth: number = 3): (depth?: number) => SpecNode {
+  let id = 0;
+
+  function createNode(depth: number = 0): SpecNode {
+    const node: SpecNode = {
+      children: [],
+      id: id.toString(),
+      name: `Name ${id}`,
+    };
+
+    id += 1;
+
+    if (depth === maxDepth) {
+      return node;
+    }
+
+    // tslint:disable-next-line:no-increment-decrement
+    for (let i = 0; i < 5; i++) {
+      node.children.push(createNode(depth + 1));
+    }
+
+    return node;
+  }
+
+  return createNode;
+}
+
+export function createNodeGetter({
+  isOpenedByDefault = true,
+  root = createNodeCreator()(),
+}: NodeGetterOptions = {}): (refresh: boolean) => IterableIterator<Node> {
+  // tslint:disable-next-line:no-function-expression
+  return function* nodeGetter(refresh: boolean): IterableIterator<Node> {
+    interface StackElement {
+      node: SpecNode;
+      nestingLevel: number;
+    }
+
+    const stack = [];
+
+    stack.push({
+      nestingLevel: 0,
+      node: root,
+    });
+
+    while (stack.length !== 0) {
+      const {node, nestingLevel}: StackElement = stack.pop()!;
+
+      const isOpened = refresh ? yield {
+        childrenCount: node.children.length,
+        id: node.id,
+        isOpenedByDefault,
+        nestingLevel,
+        nodeData: node.name,
+      } : node.id;
+
+      if (node.children.length !== 0 && isOpened) {
+        // tslint:disable-next-line:no-increment-decrement
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push({
+            nestingLevel: nestingLevel + 1,
+            node: node.children[i],
+          });
+        }
+      }
+    }
+  }
+}
+
+function defaultRowRenderer({key, isOpened, nodeData, onNodeToggle, style}: RowRendererParams): React.ReactElement<any> {
+  return (
+    <div className="treeNode" key={key} style={style}>
+      <button onClick={onNodeToggle}>{isOpened ? '[-]' : '[+]'}</button>
+      <span>{String(nodeData)}</span>
+    </div>
+  );
+}
+
+export function createTreeRenderer({
+  nodeGetter,
+  rowRenderer = defaultRowRenderer,
+}: TreeRendererOptions): (props?: Partial<TreeProps>) => ReactWrapper<TreeProps, any> {
+  return (props = {}) => mount(
+    <Tree
+      height={100}
+      overscanIndicesGetter={overscanIndicesGetter}
+      overscanRowCount={0}
+      nodeGetter={nodeGetter}
+      rowRenderer={rowRenderer}
+      rowHeight={10}
+      width={100}
+      {...props}
+    />,
+  );
+}
+
+export class MockMethod<T> {
+  private originalMethod: Function;
+  private mockedMethod: jest.Mock<T>;
+
+  public constructor(
+    private cls: any,
+    private method: string,
+  ) {
+    this.originalMethod = this.cls[this.method];
+    this.setUp();
+  }
+
+  public get info(): jest.MockContext<T> {
+    return this.mockedMethod.mock;
+  }
+
+  public setUp(): void {
+    this.mockedMethod = jest.fn(this.cls[this.method]);
+    this.cls[this.method] = this.mockedMethod;
+  }
+
+  public clear(): void {
+    this.cls[this.method] = this.originalMethod;
+  }
+}
--- config/jest/enzyme.js
+++ config/jest/enzyme.js
@@ -1,5 +1,4 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-require('jest-enzyme');
 
 Enzyme.configure({adapter: new Adapter()});
