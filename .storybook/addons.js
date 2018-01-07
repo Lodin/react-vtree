3aaad077f586f8475ade0f6ebc09c9fef80064e2
Vlad Rindevich <rindevich.vs@gmail.com>
2018-01-08 00:35:00 +0300

    gh-pages: add storybook

--- .storybook/addons.js
+++
@@ -1,3 +0,0 @@
-require('@storybook/addon-actions/register');
-require('@storybook/addon-knobs/register');
-require('@storybook/addon-options/register');
--- src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css' {
-  const content: {[key: string]: string};
-  export = content;
-}
-
-declare module 'fbjs/lib/shallowEqual' {
-  function shallowEqual<T extends object, U extends object>(objA: T, objB: U): boolean;
-  export = shallowEqual;
-}
