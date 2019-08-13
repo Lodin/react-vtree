a52b1d9ac84099c2882822936a829aca91e17347
Vlad Rindevich <rindevich.vs@gmail.com>
2019-08-14 01:47:40 +0300

    docs: add documentation site

---
+++ docs/main.b51de0b427b8cee5563e.bundle.js
@@ -0,0 +1,12 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    438: function(n, o, p) {
+      p(439), p(542), (n.exports = p(920));
+    },
+    542: function(n, o, p) {
+      p(543), p(917);
+    },
+  },
+  [[438, 1, 2]],
+]);
