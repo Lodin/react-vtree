c00c551ff5c9e848009d474d77131dd62d6ab329
Vlad Rindevich <rindevich.vs@gmail.com>
2020-09-27 01:52:19 +0300

    build: update config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   setupFilesAfterEnv: ['./config/enzyme.js'],
+  testMatch: ['<rootDir>/__tests__/**/*.spec.tsx'],
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
